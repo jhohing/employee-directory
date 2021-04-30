0f082e4dfb33c7d62c34318e53d2d978e1ba450b
Justin Hohing <jnh0627@yahoo.com>
2021-04-30 06:54:22 -0400

    added randomuser API and deleted Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EmployeeCard from './components/EmployeeCard';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
